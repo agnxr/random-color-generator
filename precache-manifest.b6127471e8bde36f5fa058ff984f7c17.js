7793fdb5f3832d17d8cdd978188baa9fb0135180
agnxr <ar21@o2.pl>
2019-05-02 00:03:10 +0200

    Updates

---
+++ precache-manifest.b6127471e8bde36f5fa058ff984f7c17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f1a7d6aad84c24bc5e40c1ebac2e74f",
+    "url": "/random-color-generator/index.html"
+  },
+  {
+    "revision": "c5a24c3743779e1e71d0",
+    "url": "/random-color-generator/static/css/main.2ed1c657.chunk.css"
+  },
+  {
+    "revision": "e3e1726d43da044c5a6a",
+    "url": "/random-color-generator/static/js/2.7cddf10a.chunk.js"
+  },
+  {
+    "revision": "c5a24c3743779e1e71d0",
+    "url": "/random-color-generator/static/js/main.527d4663.chunk.js"
+  },
+  {
+    "revision": "8995effda93eb4756298",
+    "url": "/random-color-generator/static/js/runtime~main.74931d37.js"
+  }
+]);
